bd6863e91e255e20a9bf7f2fe60c5b7bc702723c
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-07-03 14:04:28 -0400

    Improve readability

--- Run.java
+++ Run.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * 
+ *
  * This class opens a file dialog box so that user may choose file for parsing.
  * Parser and Gui classes parse the file and create a GUI of the output.
- * 
+ *
  *******************************************************************************/
 
 
@@ -13,40 +13,29 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-
-
 public class Run {
-
 	public static void main(String[] args) {
-		
-
+		// ask for input file
 		JOptionPane.showMessageDialog(null, "Choose a file to begin parsing.");
-		
-		
-		File file = null; 
+		File file = null;
 		JFileChooser fileChooser = new JFileChooser(".");
 		int fileChoice = fileChooser.showOpenDialog(null);
-		
-		
+
 		if ( fileChoice == JFileChooser.APPROVE_OPTION){
 			file = fileChooser.getSelectedFile();
 		}
-		
 		file = new File("TestFile.txt");
-		
-	
+
+		// read data from file
 		Scanner scn;
 		try {
 			scn = new Scanner(file);
 			Parser p = new Parser(scn);
-			
 			Gui g = new Gui(p);
-			
 			g.buildWindow();
-
 			scn.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}		
+		}
 	}
 }
